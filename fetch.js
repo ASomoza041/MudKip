9c967029f4750000a234accf71507d22d9371edb
Alex Somoza <asomoza041@west-mec.org>
2019-09-25 18:25:11 -0700

    xml and fetch request
    Made requests to api

---
+++ fetch.js
@@ -0,0 +1,21 @@
+
+//Where it is fetching the data from
+const fetchPromise = fetch("https://ghibliapi.herokuapp.com/people");
+//The element in the HTML doucment in which the data is being put into 
+const main = document.getElementById("main");
+
+//
+fetchPromise.then(response => {
+  return response.json();
+}).then(people => {
+  const names = people.map(person => person.name).join("\n");
+  main.innerHTML = listNames(people);
+});
+//Organizes the list into a readable format
+function listNames(people) {
+    const names = people.map(person => `<li>${person.name}</li>`).join("\n");
+    return`<ul>${names}</ul>`
+}
+ 
+
+
